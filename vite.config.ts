9bb20c6b85b162c818a2fffe6245d9de25309fff
FoxyGDFD <mic.panfil0v@gmail.com>
2025-09-16 22:04:16 +0300

    feat: setup sass

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,15 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-        '@app': path.resolve(__dirname, './src/app'),
-        '@pages': path.resolve(__dirname, './src/pages'),
-        '@widgets': path.resolve(__dirname, './src/widgets'),
-        '@entities': path.resolve(__dirname, './src/entities'),
-        '@features': path.resolve(__dirname, './src/features'),
-        '@shared': path.resolve(__dirname, './src/shared'),
-        '@env': path.resolve(__dirname, './src/shared/lib/env'),
-      },
-    },
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@app': path.resolve(__dirname, './src/app'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+			'@entities': path.resolve(__dirname, './src/entities'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@shared': path.resolve(__dirname, './src/shared'),
+			'@env': path.resolve(__dirname, './src/shared/lib/env'),
+		},
+	},
 })
