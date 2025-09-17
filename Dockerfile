b0b10d64284d8dbb581a19b4881b5b1e3ca5bec3
FoxyGDFD <mic.panfil0v@gmail.com>
2025-09-18 00:57:06 +0300

    refactor docker bootstrap

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,15 @@ FROM node:24-alpine AS app
 
 WORKDIR /app
 
-COPY package*.json .
+COPY package*.json ./
 RUN npm i
 
 COPY . ./
 
 
-
 FROM app AS development
-
-WORKDIR /app
-
 CMD [ "npm", "run", "dev" ]
+
 
 
 FROM app AS production
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,29 @@
 import path from 'path';
-import svgr from "vite-plugin-svgr";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-// https://vite.dev/config/
+import { defineConfig, loadEnv } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
-export default defineConfig({
-  plugins: [react(), svgr({ include: "**/*.svg?react" })],
-  resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@app': path.resolve(__dirname, './src/app'),
-			'@pages': path.resolve(__dirname, './src/pages'),
-			'@widgets': path.resolve(__dirname, './src/widgets'),
-			'@entities': path.resolve(__dirname, './src/entities'),
-			'@features': path.resolve(__dirname, './src/features'),
-			'@shared': path.resolve(__dirname, './src/shared'),
-			'@env': path.resolve(__dirname, './src/shared/lib/env'),
-		},
-	},
-})
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), svgr({ include: '**/*.svg?react'})],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@app': path.resolve(__dirname, './src/app'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@widgets': path.resolve(__dirname, './src/widgets'),
+        '@entities': path.resolve(__dirname, './src/entities'),
+        '@features': path.resolve(__dirname, './src/features'),
+        '@shared': path.resolve(__dirname, './src/shared'),
+				'@env': path.resolve(__dirname, './src/shared/lib/env'),
+      },
+    },
+    server: {
+      port: parseInt(env.PORT) || 5173,
+    },
+  };
+});
